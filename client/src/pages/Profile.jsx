6f915d0d00ed0d33fc22f3d11c113f847c3508e8
ananyakumari1293 <ananyakumari1293@gmail.com>
2026-05-25 12:28:02 +0530

    Added dynamic saved scholarship count

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,57 @@
 import "./Profile.css";
 
+import {
+  useEffect,
+  useState
+} from "react";
+
 import { useAuth }
 from "../context/AuthContext";
 
 function Profile() {
 
-  const { currentUser } = useAuth();
+  const { currentUser } =
+    useAuth();
+
+  const [savedCount, setSavedCount] =
+    useState(0);
+
+  useEffect(() => {
+
+    const fetchSavedScholarships =
+      async () => {
+
+        if (!currentUser) return;
+
+        try {
+
+          const response =
+            await fetch(
+
+              `${import.meta.env.VITE_API_URL}/api/saved/${currentUser.email}`
+
+            );
+
+          const data =
+            await response.json();
+
+          setSavedCount(
+            data.length
+          );
+
+        } catch (error) {
+
+          console.log(error);
+
+        }
+      };
+
+    fetchSavedScholarships();
+
+  }, [currentUser]);
 
   return (
+
     <div className="profile-page">
 
       <div className="profile-card">
@@ -15,7 +59,11 @@ function Profile() {
         <div className="profile-top">
 
           <div className="profile-avatar">
-            {currentUser?.email?.charAt(0).toUpperCase()}
+
+            {currentUser?.email
+              ?.charAt(0)
+              .toUpperCase()}
+
           </div>
 
           <div>
@@ -65,7 +113,7 @@ function Profile() {
             </p>
 
             <h3>
-              0 Saved
+              {savedCount} Saved
             </h3>
 
           </div>
@@ -73,6 +121,7 @@ function Profile() {
         </div>
 
       </div>
+
     </div>
   );
 }
